87aeb7ff5d27cfb7736f9d63dd16036b79efbc51
victorsplat <victorprado123@gmail.com>
2026-05-10 21:13:20 -0300

    fix: remove .env.local from git tracking, revert unnecessary TLS options, update gitignore to .env*

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -2,22 +2,17 @@ import { MongoClient } from 'mongodb';
 
 const uri = process.env.MONGODB_URI;
 
-const options = {
-  tls: true,
-  tlsAllowInvalidCertificates: process.env.NODE_ENV === 'development',
-};
-
 let client;
 let clientPromise;
 
 if (process.env.NODE_ENV === 'development') {
   if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
+    client = new MongoClient(uri);
     global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise;
 } else {
-  client = new MongoClient(uri, options);
+  client = new MongoClient(uri);
   clientPromise = client.connect();
 }
 
